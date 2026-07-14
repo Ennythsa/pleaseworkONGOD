f059cc938897dac47b201bd4f46ccf7e48bc0c8e
Ennythsa <135762360+Ennythsa@users.noreply.github.com>
2026-07-14 22:12:16 +0800

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -3,3 +3,18 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello JavaScript!</h1>
 `;
+
+const overlayHair = document.getElementById("overlayHair");
+
+const overlayBtn = document.getElementById("overlayBtn");
+const offBtn = document.getElementById("offBtn");
+
+
+overlayBtn.onclick = () => {
+  overlayHair.style.display = "block";
+};
+
+
+offBtn.onclick = () => {
+  overlayHair.style.display = "none";
+};
